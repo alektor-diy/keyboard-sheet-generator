c775090649b8a0dae648aa7540c5931a5bd3d1f4
アレくとーる <alektordiy@gmail.com>
2026-09-18 02:08:20 +0900

    update v1.2.0

--- js/storage.js
+++ js/storage.js
@@ -1,6 +1,6 @@
 (function(){
   const KEY='keyboard-sheet-generator:v1';
-  const blankSheet=()=>({id:null,keyboardName:'',exhibitor:'',switchName:'',keycaps:'',typing:'',photo:'',description:''});
+  const blankSheet=()=>({id:null,keyboardName:'',exhibitor:'',switchName:'',keycaps:'',feature:'',typing:'',photo:'',description:''});
   window.SheetStorage={
     blankSheet,
     load(){try{const value=JSON.parse(localStorage.getItem(KEY));if(value&&value.version===1)return {version:1,template:['blank','nagare','tenkey','ttt'].includes(value.template)?value.template:'blank',showEventLogo:value.showEventLogo!==false,current:{...blankSheet(),...(value.current||{})},sheets:Array.isArray(value.sheets)?value.sheets:[]};}catch(e){}return {version:1,template:'blank',showEventLogo:true,current:blankSheet(),sheets:[]};},
